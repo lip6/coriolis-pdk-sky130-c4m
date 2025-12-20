fd7070c2f0f6a464fc3971a4d9501152adcc2a51
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-12-20 18:33:47 +0100

    Added support for nightly build.

--- build-LIP6.sh
+++ build-LIP6.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-    rootDir="${HOME}/coriolis-2.x"
-   buildDir="${rootDir}/release/build-sky130-c4m"
+ nightlyInstall="false"
+ while [ $# -gt 0 ]; do
+   case $1 in
+     --nightly) echo "Installing in nightly mode.";
+                      nightlyInstall="true";;
+   esac
+   shift
+ done
+
+ if [ "${nightlyInstall}" = "true" ]; then
+   rootDir="${HOME}/nightly/coriolis-2.x"
+ else
+   rootDir="${HOME}/coriolis-2.x"
+ fi
+   buildDir="${rootDir}/release/build-sky130_c4m"
  installDir="${rootDir}/release/install"
  rm -rf ${buildDir}
  rm -rf ${installDir}/lib64/python3.9/site-packages/pdks/sky130_c4m
